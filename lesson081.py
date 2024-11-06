9d3cf76881b58768fa40af31329761282ff337f1
Mark Peters <mapeters101@earthlink.net>
2024-11-06 12:00:57 -0500

    Lesson081: Work on lesson from 8:20 to 11:50.

--- lesson081.py
+++ lesson081.py
@@ -36,10 +36,14 @@ print('+'*10)
 
 x = 3
 y = 5
-def my_func3(a):
+def my_func3(a: 'some character', b=max(x,y)) -> 'character a repeated ' + str(max(x,y)) + ' times':
+    print(b)
     return a * max(x, y)
 
 print(my_func3('a'))
+print(my_func3.__annotations__)
 
-
+x = 10
+print(my_func('a'))
+print(my_func3.__annotations__)
 
