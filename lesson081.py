937dcab649d1bb669fa63e5a19532645b0d1dcaf
Mark Peters <mapeters101@earthlink.net>
2024-11-06 12:49:04 -0500

    Lesson081: Complete work on lesson from 11:50.

--- lesson081.py
+++ lesson081.py
@@ -46,4 +46,20 @@ print(my_func3.__annotations__)
 x = 10
 print(my_func('a'))
 print(my_func3.__annotations__)
+print('='*20)
+def my_func4(a: str,
+            b: 'int > 0' = 1,
+            *args: 'some extra positional args',
+            k1: 'keyword-only arg 1',
+            k2: 'keyword-only arg 2' = 100,
+            **kwargs: 'some extra keyword-only args') -> 'something':
+    print(a, b, args, k1, k2, kwargs)
+
+help(my_func4)
+print(my_func4.__annotations__)
+
+
+my_func4(1, 2, 3, 4, 5, k1=10, k3=300, k4=400)
+
+
 
