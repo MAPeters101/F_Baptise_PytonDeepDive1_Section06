149c7513c37b4d3d10fd5f584151317a8a351e7f
Mark Peters <mapeters101@earthlink.net>
2024-11-06 15:45:33 -0500

    Lesson083: Complete work on lesson from 7:45.

--- lesson083.py
+++ lesson083.py
@@ -28,3 +28,30 @@ print(f(1, 'a', 'b', y=100, a=10, b=20))
 f = lambda x, *args, y, **kwargs: (x, *args, y, kwargs)
 print(f(1, 'a', 'b', y=100, a=10, b=20))
 
+def apply_func(x, fn):
+    return fn(x)
+
+print(apply_func(3, sq))
+
+print()
+
+print(apply_func(5, lambda x: x**2))
+print(apply_func(5, lambda x: x**3))
+
+def apply_func2(fn, *args, **kwargs):
+    return fn(*args, **kwargs)
+
+print(apply_func2(sq, 3))
+print(apply_func2(lambda x: x**2, 3))
+print(apply_func2(lambda x, y: x + y, 1, 2))
+print(apply_func2(lambda x, *, y: x + y, 1, y=4))
+print(apply_func2(lambda *args: sum(args), 1, 2, 3, 4, 5))
+print(apply_func2(sum, (1, 2, 3, 4, 5)))
+print(sum((1, 2, 3, 4, 5, 6)))
+
+
+
+
+
+
+
