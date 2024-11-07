180c2dcce4f7b73f5141dd5f50925897af4dfe60
Mark Peters <mapeters101@earthlink.net>
2024-11-06 19:23:26 -0500

    Lesson088: Complete work on lesson from 9:50.

--- lesson088.py
+++ lesson088.py
@@ -22,6 +22,36 @@ a = Decimal('10.5')
 print(a)
 print(type(a))
 print(callable(a))
+print('='*20)
 
+class MyClass:
+    def __init__(self, x=0):
+        print('initializing....')
+        self.counter = x
 
+print(callable(MyClass))
+a = MyClass(100)
+print(a.counter)
+print(callable(a))
+print('='*20)
+
+class MyClass2:
+    def __init__(self, x=0):
+        print('initializing....')
+        self.counter = x
+
+    def __call__(self, x=1):
+        print('updating counter...')
+        self.counter += x
+
+b = MyClass2()
+MyClass2.__call__(b, 10)
+print(b.counter)
+print(callable(b))
+b()
+b(100)
+print(b.counter)
+
+print(type(MyClass))
+print(type(Decimal))
 
