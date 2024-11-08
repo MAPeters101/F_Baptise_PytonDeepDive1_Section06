057d0b2e6044c80040e0f78084e46f82560d9f88
Mark Peters <mapeters101@earthlink.net>
2024-11-08 09:46:43 -0500

    Lesson090: Work on lesson from 6:45 to 13:00.

--- lesson090.py
+++ lesson090.py
@@ -44,3 +44,47 @@ print(results)
 #     print(x)
 print('2'*20)
 
+x = range(5)
+print(x)
+
+for i in x:
+    print(i)
+
+for i in x:
+    print(i)
+
+print(list(filter(lambda x: x%3 == 0, range(25))))
+print(filter(None, [1, 0, 4, 'a', '', None, True, False]))
+print(list(filter(None, [1, 0, 4, 'a', '', None, True, False])))
+print()
+
+l1 = [1, 2, 3, 4]
+l2 = [10, 20, 30, 40]
+l3 = 'python'
+results = zip(l1, l2, l3)
+print(results)
+print('3-'*10)
+for x in results:
+    print(x)
+for x in results:
+    print(x)
+print('4-'*10)
+print()
+
+results = list(zip(l1, l2, l3))
+print(results)
+print('3-'*10)
+for x in results:
+    print(x)
+for x in results:
+    print(x)
+print('4-'*10)
+print()
+
+result = list(zip(range(10000), 'python'))
+print(result)
+
+
+
+
+
