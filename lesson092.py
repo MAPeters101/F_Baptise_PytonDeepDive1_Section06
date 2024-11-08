6a2bf2d4a23856167d3e36c233cdf6bfb32b488b
Mark Peters <mapeters101@earthlink.net>
2024-11-08 11:10:55 -0500

    Lesson092: Work on lesson from 4:50 to 10:45.

--- lesson092.py
+++ lesson092.py
@@ -40,3 +40,46 @@ def add_sequence(sequence):
 print(add_sequence(l))
 print()
 
+def _reduce(fn, sequence):
+    result = sequence[0]
+    for x in sequence[1:]:
+        result = fn(result, x)
+    return result
+
+print(_reduce(_max, l))
+print(_reduce(_min, l))
+print(_reduce(_add, l))
+print()
+
+#print(_reduce(_add, {1,2,3,4,5}))
+
+from functools import reduce
+
+print(reduce(_max, l))
+print(reduce(_min, l))
+print(reduce(_add, l))
+print()
+
+print(reduce(_max, {1,2,3,4,5}))
+print(reduce(_min, {1,2,3,4,5}))
+print(reduce(_add, {1,2,3,4,5}))
+print()
+
+print(min(l))
+print(max(l))
+print(sum(l))
+print(min({1,2,3,4,5}))
+print(max({1,2,3,4,5}))
+print(sum({1,2,3,4,5}))
+print()
+
+s = {True, 1, 0, None}
+print(all(s))
+
+s2 = {True, 1, 's'}
+print(all(s2))
+print(bool(True) and bool(1) and bool('s'))
+print()
+
+print(any(s))
+print(any(s2))
